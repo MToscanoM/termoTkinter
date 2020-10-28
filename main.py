5c18d2122c81412d017d94d9194b2361397525a2
Mariano Toscano <marianotoscano@outlook.com>
2020-10-28 12:33:47 +0100

    Redefinir ventana y colocar entrada de texto

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ from tkinter import *
 from tkinter import ttk
 
 class mainApp(Tk):
-    size = "640x480"
+    entrada = None
+    tipoUnidad = None
     
     def __init__(self):
         Tk.__init__(self)
         
-        self.geometry(self.size)
-        self.title("Mi ventana")
-        self.configure(bg = "blue")
+        self.geometry("200x150")
+        self.title("Termómetro")
+        self.configure(bg="#DED7B9")
+        
+        self.temperatura = StringVar(value="")
+        self.tipoUnidad = StringVar(value="C")
+        
+        self.createLayout()
+        
+    def createLayout(self):
+        self.entrada = ttk.Entry(self, textvariable=self.temperatura).place(x=10, y=10)
         
     def start(self):
         self.mainloop()
