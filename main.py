6dd89b63001bfce044569fb51c85cc8a821e987b
Mariano Toscano <marianotoscano@outlook.com>
2020-10-28 13:07:23 +0100

    Colocar Radiobutton y recolocar elementos

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ class mainApp(Tk):
     def __init__(self):
         Tk.__init__(self)
         
-        self.geometry("200x150")
+        self.geometry("210x150")
         self.title("Termómetro")
-        self.configure(bg="#DED7B9")
+        self.configure(bg="#ECECEC")
+        self.resizable(0,0)
         
         self.temperatura = StringVar(value="")
+        self.temperatura.trace("w", self.validateTemperature)
         self.tipoUnidad = StringVar(value="C")
         
         self.createLayout()
@@ -20,8 +22,16 @@ class mainApp(Tk):
     def createLayout(self):
         self.entrada = ttk.Entry(self, textvariable=self.temperatura).place(x=10, y=10)
         
+        self.lblUnidad = ttk.Label(self, text="Grados:").place(x=10, y=50)
+    
+        self.rb1 = ttk.Radiobutton(self, text="Fahrenheit", variable=self.tipoUnidad, value="F").place(x=20, y=70)
+        self.rb2 = ttk.Radiobutton(self, text="Celsius", variable=self.tipoUnidad, value="C").place(x=20, y=95)
+
     def start(self):
         self.mainloop()
+        
+    def validateTemperature(self, *args):
+        print(self.temperatura.get())
 
 
 if __name__ == '__main__':
